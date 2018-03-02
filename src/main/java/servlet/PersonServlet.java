08c2fb515ed4a45ae3068a5b04375d5ef65a7a3d
hzq0604 <hzq15283598325>
2018-03-02 09:14:41 +0800

    修改商铺servlet以及完善个人信息修改和收货地址维护功能

--- src/main/java/servlet/PersonServlet.java
+++ src/main/java/servlet/PersonServlet.java
@@ -8,7 +8,9 @@ package servlet;/***************************************************************
  * @version V1.0
  */
 
+import common.util.SpringContextUtil;
 import domain.People;
+import service.PeopleService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -39,25 +41,31 @@ public class PersonServlet extends HttpServlet{
 //        设置字符集
         req.setCharacterEncoding("utf-8");
         resp.setCharacterEncoding("utf-8");
-//        给对象赋值
-        String userName=req.getParameter("username");
-        String name=req.getParameter("name");
-        String idd=req.getParameter("cardId");
-        String pwd=req.getParameter("password");
-        String Sex=req.getParameter("sex");
-        String Age=req.getParameter("age");
-        String Money=req.getParameter("money");
-        String add=req.getParameter("address");
-        System.out.println("userName"+" "+pwd);//
+//      先查找出要修改的人员信息
+        Long peopleId = Long.valueOf(req.getParameter("peopleId"));
+        PeopleService peopleService = (PeopleService) SpringContextUtil.getBean("peopleService");
+        People people = new People();
+        people = peopleService.findPeopleById(peopleId);
+        //判断用户修改的内容
+        String address = req.getParameter("address");
+        String password = req.getParameter("password");
+        if (address.equals(people.getPassWord())){
+            if (password.equals(people.getPassWord())){
+                System.out.println("请输入要修改的内容");
+            }
+        }else
+        {
+            People newPeople = new People();
+            newPeople.setPassWord(password);
+            newPeople.setAddress(address);
+            int row=peopleService.updatePeople(newPeople);
+            if(row!=0){
+                System.out.println("修改成功");
+                //跳转到响应的servlet
+                resp.sendRedirect("/");
+            }
+        }
 
-//        将person信息存入数据库，调用peopleService的save方法
-        People person=new People();
-        person.setUsreName(userName);
-        person.setName(name);
-        person.setSex(Sex);
-        person.setCardId(idd);
-        person.setAddress(add);
-        person.setPassWord(pwd);
     }
 
 
