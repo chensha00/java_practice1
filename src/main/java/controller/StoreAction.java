8a9778815f7c065201f786ed38e6c9e5c5dde2e5
yuweiyang009 <729328176@qq.com>
2018-03-06 11:17:24 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/chensha00/java_practice1""
    This reverts commit b010ec69702c768c2e22542e19553b45f7b0d5f6.

---
+++ src/main/java/controller/StoreAction.java
@@ -0,0 +1,20 @@
+package controller;/********************************************************************
+ /**
+ * @Project: zyht_web
+ * @Package controller
+ * @author 胡志强
+ * @date 2018/3/6 10:34
+ * @Copyright: 2018 www.zyht.com Inc. All rights reserved.
+ * @version V1.0
+ */
+
+import common.util.base.BaseAction;
+
+/**
+ * @ClassName StoreAction
+ * @Description 商铺相关action
+ * @author 胡志强
+ * @date 2018/3/6
+*/
+public class StoreAction extends BaseAction{
+}
