290673c4ee36492f92b69c886f85055c60650a53
jiangxiangwen <1144121618@qq.com>
2018-03-07 18:46:21 +0800

    添加了登陆页的提示功能

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -9,6 +9,7 @@ package servlet;/***************************************************************
  */
 
 
+import com.google.gson.Gson;
 import common.util.SpringContextUtil;
 import domain.People;
 import service.PeopleService;
@@ -19,6 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
 /**
  * @author yanyong
@@ -65,31 +68,28 @@ public class LoginServlet extends HttpServlet {
         //使用Spring调用service层
         PeopleService peopleService = (PeopleService) SpringContextUtil.getBean("peopleService");
         People person = peopleService.findPeopleByUserName(userName, pwd);
-        System.out.println(person.getUsreName() + person.getPassWord()); //
+//        System.out.println(person.getUsreName() + person.getPassWord()); //
         HttpSession session = req.getSession();
-        if (person.getId() == null) {
-            session.setAttribute("LoginMessage", "error");
-            resp.sendRedirect("../login_page.jsp");
-        } else if (person.getId() == 0L) {
+        if (person == null) {
             session.setAttribute("LoginMessage", "error");
             resp.sendRedirect("../login_page.jsp");
         } else {
             session.removeAttribute("LoginMessage");
             session.setAttribute("person", person);
-            resp.sendRedirect("/servlet/mainPage.htm");
+//            resp.sendRedirect("/servlet/mainPage.htm");
 //            resp.setCharacterEncoding("UTF-8");
 //            resp.setContentType("text/html;charset=UTF-8");
 //
-//            PrintWriter out = resp.getWriter();
+            PrintWriter out = resp.getWriter();
 //
-//            HashMap<String,String> hashMap=new HashMap<String, String>();
-//            hashMap.put("MESSAGE","登陆成功");
-//            Gson gson=new Gson();
+            HashMap<String,String> hashMap=new HashMap<String, String>();
+            hashMap.put("MESSAGE","登陆成功");
+            Gson gson=new Gson();
 //
-//            out.print(gson.toJson(hashMap));
+            out.print(gson.toJson(hashMap));
 //            //关闭流
-//            out.flush();
-//            out.close();
+            out.flush();
+            out.close();
         }
     }
 }
